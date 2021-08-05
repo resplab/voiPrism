c69cc81aab13c9927f8fd15aef917e09cf17f8a2
yzr96 <eric.yu@usask.ca>
2021-08-05 11:27:16 -0700

    add functions in bridge

--- R/bridge_prism.R
+++ R/bridge_prism.R
@@ -6,74 +6,93 @@
 #' @export
 #'
 #' @examples
-model_run<-function(model_input = NULL)
+model_run<-function(model_input = NULL, method)
 {
+  if(method=="evpi"){
+    input<-unflatten_list(model_input)
 
-  input<-unflatten_list(model_input)
+    results <- evpi         (outputs                        =model_input$outputs,
+                             nsim                           =model_input$nsim)
 
-  results <- evpi         (outputs                        =model_input$outputs,
-                           nsim                           =model_input$nsim)
-
-  return(as.list(results))
-}
-
-
-get_default_input <- function() {
-  inputs <- data.frame(p1 = rnorm(nsam, 1, 1),
-                       p2 = rnorm(nsam, 0, 2))
-
-  outputs_nb <- data.frame(t1 = 0,
-                           t2 = inputs$p1 - inputs$p2)
-  model_input <- list(outputs                              =outputs_nb,
-                      nsim                                 =NULL)
-  return((flatten_list(model_input)))
-}
-
-
-#Gets a hierarchical named list and flattens it; updating names accordingly
-flatten_list<-function(lst,prefix="")
-{
-  if(is.null(lst)) return(lst)
-  out<-list()
-  if(length(lst)==0)
-  {
-    out[prefix]<-NULL
-    return(out)
+    return(as.list(results))
   }
 
-  for(i in 1:length(lst))
-  {
-    nm<-names(lst[i])
+  if(method=="evppi"){
+    input<-unflatten_list(model_input)
 
-    message(nm)
+    results <- evppi         (outputs                        =model_input$outputs,
+                              inputs                         =model_input$inputs,
+                              pars                           =model_input$pars,
+                              method                         =model_input$method,
+                              se                             =model_input$se,
+                              B                              =model_input$B,
+                              nsim                           =model_input$nsim,
+                              verbos                         =model_input$verbos,
+                              ...)
 
-    if(prefix!="")  nm<-paste(prefix,nm,sep=".")
-
-    if(is.list(lst[[i]]))
-      out<-c(out,flatten_list(lst[[i]],nm))
-    else
-    {
-      out[nm]<-lst[i]
-    }
-  }
-  return(out)
-}
-
-
-
-#Gets a hierarchical named list and flattens it; updating names accordingly
-unflatten_list<-function(lst)
-{
-  if(is.null(lst)) return(lst)
-  out<-list()
-
-  nms<-names(lst)
-
-  for(nm in nms)
-  {
-    path<-paste(strsplit(nm,'.',fixed=T)[[1]],sep="$")
-    eval(parse(text=paste("out$",paste(path,collapse="$"),"<-lst[[nm]]",sep="")))
+    return(as.list(results))
   }
 
-  return(out)
+  if(method=="evppi_mc"){
+    input<-unflatten_list(model_input)
+
+    results <- evppi_mc      (model_fn                       =model_input$model_fn,
+                              par_fn                         =model_input$par_fn,
+                              pars                           =model_input$pars,
+                              nouter                         =model_input$nouter,
+                              ninner                         =model_input$ninner,
+                              wtp                            =model_input$wtp,
+                              mfargs                         =model_input$mfargs,
+                              verbos                         =model_input$verbos)
+
+    return(as.list(results))
+  }
+
+  if(method=="evsi"){
+    input<-unflatten_list(model_input)
+
+    results <- evsi          (outputs                        =model_input$outputs,
+                              inputs                         =model_input$inputs,
+                              study                          =model_input$study,
+                              datagen_fn                     =model_input$datagen_fn,
+                              pars                           =model_input$pars,
+                              n                              =model_input$n,
+                              method                         =model_input$method,
+                              likelihood                     =model_input$likelihood,
+                              analysis_model                 =model_input$analysis_model,
+                              analysis_options               =model_input$analysis_options,
+                              decision_model                 =model_input$decision_model,
+                              Q                              =model_input$Q,
+                              npreg_method                   =model_input$npreg_method,
+                              nsim                           =model_input$nsim,
+                              verbos                         =model_input$verbos,
+                              ...)
+
+    return(as.list(results))
+  }
+
+  if(method=="evsivar"){
+    input<-unflatten_list(model_input)
+
+    results <- evsivar       (outputs                        =model_input$outputs,
+                              inputs                         =model_input$inputs,
+                              study                          =model_input$study,
+                              datagen_fn                     =model_input$datagen_fn,
+                              pars                           =model_input$pars,
+                              n                              =model_input$n,
+                              method                         =model_input$method,
+                              likelihood                     =model_input$likelihood,
+                              analysis_model                 =model_input$analysis_model,
+                              analysis_options               =model_input$analysis_options,
+                              decision_model                 =model_input$decision_model,
+                              Q                              =model_input$Q,
+                              npreg_method                   =model_input$npreg_method,
+                              nsim                           =model_input$nsim,
+                              verbos                         =model_input$verbos,
+                              ...)
+
+    return(as.list(results))
+  }
+
 }
+
