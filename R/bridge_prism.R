a25a4e05b58f2f2aca7a19674bdc5c20c23be3ca
William Xu <yihangxu001@hotmail.com>
2021-08-14 16:55:02 -0700

    Update with do call function to deal with unspecified arguments

--- R/bridge_prism.R
+++ R/bridge_prism.R
@@ -18,15 +18,16 @@ model_run<-function(model_input = NULL)
 
   if(model_input$func=="evppi"){
 
-    results <- evppi         (outputs                        =model_input$outputs,
-                              inputs                         =model_input$inputs,
-                              pars                           =model_input$pars,
-                              method                         =model_input$method,
-                              se                             =model_input$se,
-                              B                              =model_input$B,
-                              nsim                           =model_input$nsim,
-                              verbos                         =model_input$verbos,
-                              ...                            =model_input$etc)
+    args <- list(outputs                        =model_input$outputs,
+                 inputs                         =model_input$inputs,
+                 pars                           =model_input$pars,
+                 method                         =model_input$method,
+                 se                             =model_input$se,
+                 B                              =model_input$B,
+                 nsim                           =model_input$nsim,
+                 verbos                         =model_input$verbos)
+
+    results <- do.call(evppi, c(args,model_input$etc))
 
     return(as.list(results))
   }
@@ -47,44 +48,46 @@ model_run<-function(model_input = NULL)
 
   if(model_input$func=="evsi"){
 
-    results <- evsi          (outputs                        =model_input$outputs,
-                              inputs                         =model_input$inputs,
-                              study                          =model_input$study,
-                              datagen_fn                     =model_input$datagen_fn,
-                              pars                           =model_input$pars,
-                              n                              =model_input$n,
-                              method                         =model_input$method,
-                              likelihood                     =model_input$likelihood,
-                              analysis_model                 =model_input$analysis_model,
-                              analysis_options               =model_input$analysis_options,
-                              decision_model                 =model_input$decision_model,
-                              Q                              =model_input$Q,
-                              npreg_method                   =model_input$npreg_method,
-                              nsim                           =model_input$nsim,
-                              verbos                         =model_input$verbos,
-                              ...                            =model_input$etc)
+    args <- list(outputs                        =model_input$outputs,
+                 inputs                         =model_input$inputs,
+                 study                          =model_input$study,
+                 datagen_fn                     =model_input$datagen_fn,
+                 pars                           =model_input$pars,
+                 n                              =model_input$n,
+                 method                         =model_input$method,
+                 likelihood                     =model_input$likelihood,
+                 analysis_model                 =model_input$analysis_model,
+                 analysis_options               =model_input$analysis_options,
+                 decision_model                 =model_input$decision_model,
+                 Q                              =model_input$Q,
+                 npreg_method                   =model_input$npreg_method,
+                 nsim                           =model_input$nsim,
+                 verbos                         =model_input$verbos)
+
+    results <- do.call(evsi, c(args, model_input$etc))
 
     return(as.list(results))
   }
 
   if(model_input$func=="evsivar"){
 
-    results <- evsivar       (outputs                        =model_input$outputs,
-                              inputs                         =model_input$inputs,
-                              study                          =model_input$study,
-                              datagen_fn                     =model_input$datagen_fn,
-                              pars                           =model_input$pars,
-                              n                              =model_input$n,
-                              method                         =model_input$method,
-                              likelihood                     =model_input$likelihood,
-                              analysis_model                 =model_input$analysis_model,
-                              analysis_options               =model_input$analysis_options,
-                              decision_model                 =model_input$decision_model,
-                              Q                              =model_input$Q,
-                              npreg_method                   =model_input$npreg_method,
-                              nsim                           =model_input$nsim,
-                              verbos                         =model_input$verbos,
-                              ...                            =model_input$etc)
+    args <- list(outputs                        =model_input$outputs,
+                inputs                         =model_input$inputs,
+                study                          =model_input$study,
+                datagen_fn                     =model_input$datagen_fn,
+                pars                           =model_input$pars,
+                n                              =model_input$n,
+                method                         =model_input$method,
+                likelihood                     =model_input$likelihood,
+                analysis_model                 =model_input$analysis_model,
+                analysis_options               =model_input$analysis_options,
+                decision_model                 =model_input$decision_model,
+                Q                              =model_input$Q,
+                npreg_method                   =model_input$npreg_method,
+                nsim                           =model_input$nsim,
+                verbos                         =model_input$verbos)
+
+    results <- do.call(evsivar, c(args, model_input$etc))
 
     return(as.list(results))
   }
